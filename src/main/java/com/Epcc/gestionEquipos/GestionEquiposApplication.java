71b61a1c0b77fb555d3d479145e369755affa687
hetmith01 <het.hanc.14@gmail.com>
2024-09-16 16:34:04 -0500

    inicio del projecto, configuaración inicial

---
+++ src/main/java/com/Epcc/gestionEquipos/GestionEquiposApplication.java
@@ -0,0 +1,13 @@
+package com.Epcc.gestionEquipos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEquiposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionEquiposApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Epcc/gestionEquipos/GestionEquiposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Epcc.gestionEquipos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEquiposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
