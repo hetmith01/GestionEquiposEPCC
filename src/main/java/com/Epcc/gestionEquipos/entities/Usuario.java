2b3d3769a2257c707cbb01d617bf6ec2b187e677
hetmith01 <het.hanc.14@gmail.com>
2024-09-20 17:13:15 -0500

    configuracion de la bd

---
+++ src/main/java/com/Epcc/gestionEquipos/entities/Usuario.java
@@ -0,0 +1,21 @@
+package com.Epcc.gestionEquipos.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    private String correo;
+
+    private String contrasenia;
+
+}
