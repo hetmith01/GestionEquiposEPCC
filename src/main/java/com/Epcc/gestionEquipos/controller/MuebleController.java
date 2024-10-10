4a2024f615bbcebd0f71d8c1d9cd85ba83282b2d
Hetmith <het.hanc.14@gmail.com>
2024-10-09 19:06:45 -0500

    controlador Mueble al 80%

---
+++ src/main/java/com/Epcc/gestionEquipos/controller/MuebleController.java
@@ -0,0 +1,84 @@
+package com.Epcc.gestionEquipos.controller;
+
+import com.Epcc.gestionEquipos.controller.dto.MuebleDTO;
+import com.Epcc.gestionEquipos.entities.Mueble;
+import com.Epcc.gestionEquipos.service.IMuebleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/mueble")
+public class MuebleController {
+
+    @Autowired
+    private IMuebleService muebleService
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id){
+        Optional<Mueble> muebleOptional = muebleService.findById(id);
+        if(muebleOptional.isPresent()){
+            Mueble mueble = muebleOptional.get();
+
+            MuebleDTO muebleDTO = MuebleDTO.builder()
+                    .id(mueble.getId())
+                    .codigoPatrimonial(mueble.getCodigoPatrimonial())
+                    .nombre(mueble.getNombre())
+                    .ambiente(mueble.getAmbiente())
+                    .build();
+            return ResponseEntity.ok(muebleDTO);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/findAll")
+    public ResponseEntity<?> findAll(){
+        List<MuebleDTO> muebleDTOList = muebleService.findAll()
+                .stream()
+                .map(mueble -> MuebleDTO.builder()
+                        .id(mueble.getId())
+                        .codigoPatrimonial(mueble.getCodigoPatrimonial())
+                        .nombre(mueble.getNombre())
+                        .ambiente(mueble.getAmbiente())
+                        .build())
+                .toList();
+        return ResponseEntity.ok(muebleDTOList);
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<?> save(@RequestBody MuebleDTO muebleDTO)throws URISyntaxException{
+        if(muebleDTO.getNombre().isBlank()){
+            return ResponseEntity.badRequest().build();
+        }
+
+        muebleService.save(Mueble.builder()
+                .nombre(muebleDTO.getNombre())
+                .codigoPatrimonial(muebleDTO.getCodigoPatrimonial())
+                .ambiente(muebleDTO.getAmbiente())
+                .build());
+        return ResponseEntity.created(new URI("/api/mueble/save")).build();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
